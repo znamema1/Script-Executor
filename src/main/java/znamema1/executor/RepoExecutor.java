53b2787303407c386812127d45c671d36b59a7ba
Martin Znamenáček <znamema1@fit.cvut.cz>
2017-05-04 23:55:21 +0200

    added support for error code from docker masterscript

--- src/main/java/znamema1/executor/RepoExecutor.java
+++ src/main/java/znamema1/executor/RepoExecutor.java
@@ -20,10 +20,12 @@ public class RepoExecutor {
     private static final String HOST = "localhost"; //config
     private static final int PORT = 2375; // config
     private static final String IMAGE = "test"; //config
+    private static final int RUNTIME_TIMEOUT = 5 * 1000; //config
 
     private final DockerClient client;
     private final Timer timer;
 
+    private final Integer orderNo;
     private final ArrayList<String> command;
 
     public RepoExecutor(RepoConfiguration conf, Integer id) {
@@ -39,6 +41,8 @@ public class RepoExecutor {
         command.add(conf.getComm());
         command.add(conf.getFile());
         command.add(id.toString());
+        
+        this.orderNo = conf.getOrder();
     }
 
     public void exec() throws ScriptExecutorException, DockerException, InterruptedException {// při nepodařeném clone atd - scriptexecutorexception
@@ -53,28 +57,48 @@ public class RepoExecutor {
 
         client.startContainer(creation.id());
 
+        int statusCode;
         try {
-            timer.schedule(killer, 5 * 1000);
-            client.waitContainer(creation.id());
+            timer.schedule(killer, RUNTIME_TIMEOUT);
+            statusCode = client.waitContainer(creation.id()).statusCode();
             killer.cancel();
         } catch (InterruptedException ex) {
             System.out.println("Killing container: " + creation.id());
             client.killContainer(creation.id());
-            return;
+            throw new ScriptExecutorException("Script " + orderNo + " did not finish within timeout: " + RUNTIME_TIMEOUT + "ms");
         }
-
-        // debug
-        LogStream stdout = client.logs(creation.id(), DockerClient.LogsParam.stdout());
-        String output = stdout.readFully();
-        System.out.println("Output stdout: ---");
-        System.out.print(output);
-        System.out.println("\n------------------");
-
-        LogStream stderr = client.logs(creation.id(), DockerClient.LogsParam.stderr());
-        String errput = stderr.readFully();
-        System.out.println("Output stderr: ---");
-        System.out.print(errput);
-        System.out.println("\n------------------");
+        
+        if (statusCode > 0) {
+            String errMsg = "Script " + orderNo + ": ";
+            switch (statusCode) {
+                case 1: errMsg += "Could not clone repository";
+                    break;
+                case 2: errMsg += "Could not checkout commmit";
+                    break;
+                case 3: errMsg += "Could not find file";
+                    break;
+                case 5: errMsg += "Script error: " + statusCode;
+                    break;
+                case 4:
+                case 6: 
+                default: errMsg += "Internal error";
+                    break;
+            }
+            throw new ScriptExecutorException(errMsg);
+        }
+        
+//        // debug
+//        LogStream stdout = client.logs(creation.id(), DockerClient.LogsParam.stdout());
+//        String output = stdout.readFully();
+//        System.out.println("Output stdout: ---");
+//        System.out.print(output);
+//        System.out.println("\n------------------");
+//
+//        LogStream stderr = client.logs(creation.id(), DockerClient.LogsParam.stderr());
+//        String errput = stderr.readFully();
+//        System.out.println("Output stderr: ---");
+//        System.out.print(errput);
+//        System.out.println("\n------------------");
 
         client.removeContainer(creation.id());
     }
@@ -83,5 +107,5 @@ public class RepoExecutor {
         client.close();
         timer.cancel();
     }
-
+    
 }
